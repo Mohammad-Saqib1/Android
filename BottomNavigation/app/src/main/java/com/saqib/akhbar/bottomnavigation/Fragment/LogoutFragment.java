5bc10df83442cece40606b4486162ad2093c87e3
Mohammad Saqib <eransarisaqib123@gmail.com>
2022-04-19 13:05:27 +0530

    bottom navigation

---
+++ BottomNavigation/app/src/main/java/com/saqib/akhbar/bottomnavigation/Fragment/LogoutFragment.java
@@ -0,0 +1,30 @@
+package com.saqib.akhbar.bottomnavigation.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.saqib.akhbar.bottomnavigation.R;
+
+
+public class LogoutFragment extends Fragment {
+
+
+
+    public LogoutFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_logout, container, false);
+    }
+}
---
+++ BottomNavigation/app/src/main/java/com/saqib/akhbar/bottomnavigation/Fragment/MenuFragment.java
@@ -0,0 +1,29 @@
+package com.saqib.akhbar.bottomnavigation.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.saqib.akhbar.bottomnavigation.R;
+
+
+public class MenuFragment extends Fragment {
+
+
+    public MenuFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_menu, container, false);
+    }
+}
